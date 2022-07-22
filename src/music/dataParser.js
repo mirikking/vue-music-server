040aabdae11bab84218cd7777e2c7a465eec9fbd
mirikking <mrmirik.81@gmail.com>
2022-07-22 15:12:30 +0300

    Update dataParser.js

--- src/music/dataParser.js
+++ src/music/dataParser.js
@@ -7,7 +7,6 @@ const fm = () => {
 }
 
 const names = fm()
-console.log(__dirname)
 
 const mm = require('music-metadata');
 const util = require('util');
@@ -15,18 +14,16 @@ const util = require('util');
 let songsObjectArray = [];
 
 const getSongsObject = async () => {
-  console.log('inside getting song')
   for (let i = 0; i < names.length; i++) {
+    console.log('lets try?')
     try {
       const metadata = await mm.parseFile(`${__dirname}\\${names[i]}`);
         let picture = metadata.common.picture[0];
         let encodedPicture = `data:${picture.format};base64,${picture.data.toString('base64')}`;
         let trackDuration = metadata.format.duration;
         let trackTitle = metadata.common.title;
-        console.log(trackTitle)
         let trackArtist = metadata.common.artist;
         let path = `${__dirname}\\${names[i]}`
-        console.log(`trying to catch path ${path}?`)
         let name = `${names[i]}`
         let id = Date.now();
           let trackObj = { id, name, path, trackTitle, trackArtist, trackDuration, encodedPicture }
